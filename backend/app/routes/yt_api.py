d1e39896232fcf044765ffd4547a5854929cd225
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-11 23:13:39 +0800

    feat (routes): add response model in `/api/youtube/search` API endpoint

--- backend/app/routes/yt_api.py
+++ backend/app/routes/yt_api.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter, BackgroundTasks
+from sqlalchemy.orm import selectinload
 import httpx
 from datetime import datetime
+from typing import List
 
 from app.core import config
 from app.cache.helpers import async_cache_yt_search, async_cache_videos, parse_yt_search, ytSearchResult
 from app.db import db_dependency
 from app.models import models
+from app.schema import responses as response_schema
 
 yt_router = APIRouter(prefix="/api/youtube", tags=["youtube"])
 
-@yt_router.get("/search")
+@yt_router.get("/search", response_model=List[response_schema.SearchResultResponse])
 async def yt_search(query: str, db: db_dependency, bg_task: BackgroundTasks):
     lowered_query: str = query.lower()
     now = datetime.utcnow()
@@ -17,7 +20,27 @@ async def yt_search(query: str, db: db_dependency, bg_task: BackgroundTasks):
 
     # If Exists and Not Expired
     if exists and exists.expires_at > now:
-        return exists
+        print(">>> Pulled data from DB.")
+
+        db_returned = db.query(models.SearchCacheVideo).options(
+            selectinload(models.SearchCacheVideo.video)
+                .joinedload(models.Video.channel)
+        ).filter(models.SearchCacheVideo.search_id == exists.id).all()
+
+        return [
+            response_schema.SearchResultResponse(
+                position=cache.position,
+                video_id=cache.video_id,
+                video_title=cache.video.title,
+                channel_id=cache.video.channel.channel_id,
+                channel_title=cache.video.channel.name,
+                thumbnail_url=cache.video.thumbnail_url,
+                thumbnail_width=cache.video.thumbnail_width,
+                thumbnail_height=cache.video.thumbnail_height,
+                duration_sec=cache.video.duration_sec
+            )
+            for cache in db_returned
+        ]
 
     print(">>> Query not cached yet")
 
@@ -32,7 +55,7 @@ async def yt_search(query: str, db: db_dependency, bg_task: BackgroundTasks):
         }
 
         async with httpx.AsyncClient() as client:
-            print('>> Called the YT API')
+            print('>> (api_req): Called the YT `search.list` API')
             response = await client.get(
                 url=f"{config.YOUTUBE_URL}/search",
                 params=params
@@ -53,7 +76,22 @@ async def yt_search(query: str, db: db_dependency, bg_task: BackgroundTasks):
         db=db,
     )
 
-    return parsed
+    return [
+        response_schema.SearchResultResponse(
+            position=video["position"],
+            video_id=video["video_id"],
+            video_title=video["title"],
+            channel_id=video["channel_id"],
+            channel_title=video["channel_title"],
+            thumbnail_url=video["thumbnail_url"],
+            thumbnail_width=video["thumbnail_width"],
+            thumbnail_height=video["thumbnail_height"],
+            duration_sec=None
+        )
+        for video in parsed
+    ]
+
+    # return parsed
 
 
 @yt_router.get("/video/{id}")
