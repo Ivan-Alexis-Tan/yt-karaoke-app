2820c65e97a71f603fd45c002004d170fe52a54e
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-12 11:51:31 +0800

    feat (home): add api fetch for `"/api/videos/random"` endpoint and its component UI

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,66 +10,59 @@ export default async function Home() {
   const ytVideo = await fetch(`${baseUrl}/youtube/video/EGo3a0Lbsx4`)
   const videoJson = await ytVideo.json()
 
-  const caches = await fetch("http://localhost:8000/api/test/cache")
-  const cacheJson = await caches.json()
+  const getRandomVideos = await fetch(`${baseUrl}/videos/random`)
+  const randomVideos: VideoListResponse[] = await getRandomVideos.json()
 
   console.log(`ytSearchResults =`, ytSearchResults)
   console.log("=================")
   console.log(`videoJson =`, videoJson)
   console.log("=================")
-  console.log(`cacheJson =`, cacheJson)
+  console.log(`randomVideos =`, randomVideos)
+  console.log(randomVideos[0])
   
   return (
-    <div className="flexflex-colflex-1items-centerjustify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="w-full max-w-3xl flexflex-1flex-col items-centerjustify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <iframe src="http://youtube.com/embed/EGo3a0Lbsx4"
+    <div className="bg-zinc-50 font-sans dark:bg-black">
+      <div className="px-10 w-full gap-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-start justify-between bg-white dark:bg-black">
+        {/* <iframe src="http://youtube.com/embed/EGo3a0Lbsx4"
           width={550} 
           height={500} 
           allowFullScreen
           className="mb-5"
-        ></iframe>
-      
-        {/* <table className="border-collapse [&,&_td,&_th]:border [&_td,&_th]:p-2">
-          <thead>
-            <tr>
-              {Object.keys(data[0]).map(item => (
-                <th key={item}>{item}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((row, idx) => (
-              <tr key={`c${idx}`}>
-                {Object.keys(row).map((item, i) => (
-                  <td key={`r${i}c${idx}`}>{row[item as keyof (typeof row)]}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table> */}
+        ></iframe> */}
 
-        {searchVideoInfos.length >= 1
-          ? searchVideoInfos.map(vid => {
-            const vidId = vid.id
-            const thumbnail = vid.thumbnails.medium
+        {/* {ytSearchResults.length >= 1
+          ? ytSearchResults.map(vid => {
             return (
-              <div key={vidId.videoId} className="flex gap-3 mb-5 border">
+              <div key={vid.video_id} className="flex gap-3 mb-5 border">
                 <div>
-                  <img src={thumbnail.url} alt="video thumbnail" 
-                    width={thumbnail.width} height={thumbnail.height} 
+                  <img src={vid.thumbnail_url} alt="video thumbnail" 
+                    width={vid.thumbnail_width} height={vid.thumbnail_height} 
                   />
                 </div>
 
                 <div className="gap-3 flex flex-col justify-center">
-                  <h3 className="text-xl font-bold">{vid.title}</h3>
-                  <p>{vid.channelTitle}</p>
+                  <h3 className="text-xl font-bold">{vid.video_title}</h3>
+                  <p>{vid.channel_title}</p>
                 </div>
               </div>
             )
           })
           : <p className="text-xl">No searches found</p>
-        }
-      </main>
+        } */}
+
+        {randomVideos[0].map(vid => (
+          <KaraokeVideoCard key={vid.video_id}
+            video_id={vid.video_id}
+            video_title={vid.video_title}
+            channel_id={vid.channel_id}
+            channel_title={vid.channel_title}
+            thumbnail_url={vid.thumbnail_url}
+            thumbnail_width={vid.thumbnail_width}
+            thumbnail_height={vid.thumbnail_height}
+            duration_sec={vid.duration_sec as number}
+          />
+        ))}
+      </div>
     </div>
   );
 }
