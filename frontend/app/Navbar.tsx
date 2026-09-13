ae5e2a250bbc353370d00ba3fefc6f7428a9adb4
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-13 14:37:31 +0800

    feat (navbar): add searching functionality on search bar

--- frontend/app/Navbar.tsx
+++ frontend/app/Navbar.tsx
@@ -1,7 +1,14 @@
+"use client"
+
 import SearchIcon from "@/src/svgs/SearchIcon";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Navbar({ className }: { className?: string }) {
+    const [search, setSearch] = useState("")
+    const router = useRouter()
+
     return (
         <nav className={`${className ?? ""} flex justify-between items-center`}>
             <Link href={"/"}
@@ -14,12 +21,17 @@ export default function Navbar({ className }: { className?: string }) {
                 <input type="text" 
                     title="Search a song"
                     placeholder="Search"
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
+                    onKeyUp={e => e.key === "Enter" && router.push(`/search?query=${search}`)}
                     className="pl-5 flex-1 rounded-l-2xl"
                 />
 
-                <button className="pl-3 w-20 rounded-r-2xl text-foreground hover:bg-(--light-gray-clr) bg-(--gray-clr)">
+                <Link href={`/search?query=${search}`}
+                    className="pl-3 w-20 rounded-r-2xl text-foreground hover:bg-(--light-gray-clr) bg-(--gray-clr)"
+                >
                     <SearchIcon className="w-10 h-10" />
-                </button>
+                </Link>
             </div>
 
             <div>
