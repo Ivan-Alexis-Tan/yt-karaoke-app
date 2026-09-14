c734b2a3bc1ea1d660fc386b55d22c36a5cb9d52
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-15 01:20:18 +0800

    feat (api): add `getRandomVideos()` api fetcher

--- frontend/src/api/videosApi.tsx
+++ frontend/src/api/videosApi.tsx
@@ -2,6 +2,14 @@
 
 import { BASE_URL } from "../utils/helpers";
 
+export async function getRandomVideos(videos_per_page: number = 15, limit: number = 30) {
+    const params = new URLSearchParams
+    params.append("videos_per_page", String(videos_per_page))
+    params.append("limit", String(limit))
+    
+    return await fetch(`${BASE_URL}/videos/random?${params.toString()}`)
+}
+
 export async function cacheToHistory(videoId: string) {
     await fetch(`${BASE_URL}/videos/${videoId}/history`,
         {
