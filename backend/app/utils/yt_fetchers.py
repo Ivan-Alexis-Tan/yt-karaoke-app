5bc9cfa891f8d4ca467ddea08eaaf2b6b1e1395e
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-15 01:02:35 +0800

    feat (yt fetch): add statistics part when fetching `video.list` yt api

--- backend/app/utils/yt_fetchers.py
+++ backend/app/utils/yt_fetchers.py
@@ -5,13 +5,13 @@ from app.core import config
 async def req_yt_video(id: str | list, params: dict = {}):
     params = {
         "id": id if isinstance(id, str) else ",".join(id),
-        "part": "snippet,contentDetails",
+        "part": "snippet,contentDetails,statistics",
         "key": config.API_KEY,
         **params
     }
 
     async with httpx.AsyncClient() as client:
-        print(">> Called the YT `video.list` endpoint")
+        print(">> (req_yt_video): Called the YT `video.list` endpoint")
 
         response = await client.get(
             url=f"{config.YOUTUBE_URL}/videos",
