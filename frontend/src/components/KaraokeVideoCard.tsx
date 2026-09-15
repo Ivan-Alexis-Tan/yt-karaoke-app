5510b4ff91b66b18a9e5d3e4e5e2921fabc311dc
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-16 01:42:59 +0800

    feat (component): add Karaoke video card a functionality to add to song queue

--- frontend/src/components/KaraokeVideoCard.tsx
+++ frontend/src/components/KaraokeVideoCard.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import Link from "next/link"
-import { formatMinutesSeconds } from "../utils/helpers"
 import Image from "next/image"
+import { useState } from "react";
+
+import { formatMinutesSeconds } from "../utils/helpers"
+import { addSongQueue } from "../utils/queueFn";
 import { bannedChannels } from "../appData"
+
+import AddToListIcon from "../svgs/AddToListIcon";
+import SongAddedIcon from "../svgs/SongAddedIcon";
 
 export type KaraokeVideoCardType = {
     video_id: string
@@ -13,6 +21,8 @@ export type KaraokeVideoCardType = {
     thumbnail_height: number
     duration_sec: number
     arrange?: "row" | "col"
+    disableQueBtn?: boolean
+    className?: string
 }
 
 export default function KaraokeVideoCard({
@@ -24,32 +34,71 @@ export default function KaraokeVideoCard({
     thumbnail_height,
     thumbnail_width,
     duration_sec,
-    arrange = "row"
+    arrange = "row",
+    disableQueBtn = false,
+    className,
 }: KaraokeVideoCardType) {
-    const durationInMinutes = formatMinutesSeconds(duration_sec)
+    const [songAdded, setSongAdded] = useState(false)
+
+    function addToQueue() {
+        addSongQueue({
+            video_id,
+            video_title,
+            channel_id,
+            channel_title,
+            thumbnail_url,
+            thumbnail_height,
+            thumbnail_width,
+            duration_sec,
+        })
+
+        setSongAdded(true)
+
+        setTimeout(() => {
+            setSongAdded(false)
+        }, 3000)
+    }
 
     return (
-        <Link className={`
-            ${bannedChannels.includes(channel_title) && "hidden"} 
-            ${arrange === "row" ? "" : "flex-col"}
-            p-1 max-w-130 flex gap-3 mb-5 rounded-2xl hover:bg-(--gry-700) transition-all`}
-            href={`/play/${video_id}`}
-        >
-            <div className="relative">
-                <Image className="w-full rounded-2xl" 
-                    src={thumbnail_url} alt="video thumbnail" 
-                    width={thumbnail_width} height={thumbnail_height} 
-                />
+        <div className={`${className ?? ""} karaoke-video-card relative hover:bg-(--gry-700) hover:[&_div.card-button]:flex rounded-2xl transition-all`}>
+            
+            {/* Video card information */}
+            <Link className={`
+                    gap-3 p-2 grid items-center
+                    ${bannedChannels.includes(channel_title) && "hidden"} 
+                    ${arrange === "row" ? "grid-cols-2" : "grid-cols-1"}
+                `}
+                href={`/play/${video_id}`}
+            >
+                <div className="relative">
+                    <Image className="w-full rounded-2xl" 
+                        src={thumbnail_url} alt="video thumbnail" 
+                        width={640} height={480} 
+                    />
 
-                <div className="px-0.5 absolute bottom-3 right-3 bg-(--lucent-blk-clr)">
-                    {durationInMinutes}
+                    <div className="px-0.5 absolute bottom-3 right-3 bg-(--lucent-blk-clr)">
+                        {formatMinutesSeconds(duration_sec)}
+                    </div>
                 </div>
-            </div>
 
-            <div className="m-1.5 gap-1 flex flex-col justify-center text-white">
-                <h3 className="text-xl font-bold">{video_title}</h3>
-                <p>{channel_title}</p>
-            </div>
-        </Link>
+                <div className="m-1.5 gap-1 flex flex-col justify-center text-white">
+                    <h3 className="text-xl font-bold">{video_title}</h3>
+                    <p>{channel_title}</p>
+                </div>
+            </Link>
+
+            {/* Options button */}
+            {!disableQueBtn
+                && <div className="card-button w-10 h-10 hidden absolute bottom-2 right-2 justify-center items-center hover:bg-(--lucent-blk-clr) rounded-full">
+                    {songAdded
+                        ? <SongAddedIcon className="w-7 h-7 text-green-400 font-bold" />
+                        :<button onClick={_ => addToQueue()}>
+                            <AddToListIcon className="w-7 h-7" />
+                        </button>
+                    }
+                </div>
+            }
+
+        </div>
     )
 }
