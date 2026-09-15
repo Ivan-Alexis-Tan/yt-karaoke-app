6a4ceb7fb059a7a1a35e80c05b3d8d1e57c2f426
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-16 01:37:44 +0800

    feat (component): create song queue component

---
+++ frontend/src/components/SongQueue.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+import { deleteSongQueue, emptySongQueue, getSongQueue } from "../utils/queueFn";
+
+import KaraokeVideoCard from "./KaraokeVideoCard";
+import CloseIcon from "../svgs/CloseIcon";
+import EmptyQueueIcon from "../svgs/EmptyQueueIcon";
+import DeleteIcon from "../svgs/DeleteIcon";
+
+type SongQueueParams = { 
+    closeFn: Dispatch<SetStateAction<boolean>>
+    className?: string 
+}
+export default function SongQueue({ closeFn, className }: SongQueueParams) {
+    const [queue, setQueue] = useState<VideoListResponse>(getSongQueue())
+    const [popupWindow, setPopupWindow] = useState(false)
+
+    useEffect(() => console.log(`queue =`, queue), [queue])
+
+    return (
+        <div className="max-w-150 w-[60%] h-[calc(100vh-4.5rem)] fixed right-0 top-18 rounded-l-xl bg-(--gray-clr) overflow-auto">
+            {queue.length >= 1
+                ? <div className="relative">
+                    <div className="gap-3 sticky top-0 right-0 z-(--z-navbar) bg-(--gray-clr) flex justify-end">
+                        <button onClick={_ => setPopupWindow(true)} className="hover:text-(--red-clr)">
+                            <EmptyQueueIcon className="w-7 h-7" />
+                        </button>
+                        <button className="hover:text-(--red-clr)"
+                            onClick={_ => closeFn(false)}
+                        >
+                            <CloseIcon className="w-7 h-7" />
+                        </button>
+                    </div>
+
+                    {queue.map(vid => (
+                        <div key={vid.video_id} 
+                            className="mx-3 relative hover:bg-(--light-gray-clr) hover:[&>button]:flex rounded-2xl"
+                        >
+                            <KaraokeVideoCard 
+                                video_id={vid.video_id}
+                                video_title={vid.video_title}
+                                channel_id={vid.channel_id}
+                                channel_title={vid.channel_title}
+                                thumbnail_url={vid.thumbnail_url}
+                                thumbnail_width={vid.thumbnail_width}
+                                thumbnail_height={vid.thumbnail_height}
+                                duration_sec={vid.duration_sec as number}
+                                disableQueBtn={true}
+                            />
+                            
+                            <button className="hidden w-10 h-10 absolute bottom-2 right-2 justify-center items-center hover:bg-background rounded-full"
+                                onClick={_ => {
+                                    deleteSongQueue(vid.video_id)
+                                    setQueue(getSongQueue())
+                                }}
+                            >
+                                <DeleteIcon className="w-7 h-7" />
+                            </button>
+                        </div>
+                    ))}
+                </div>
+                :<div className="w-full h-full relative flex justify-center items-center">
+                    <button className="absolute top-0 right-2 hover:text-(--red-clr)"
+                        onClick={_ => closeFn(false)}
+                    >
+                        <CloseIcon className="w-7 h-7" />
+                    </button>
+                    <div className="flex justify-center items-center">
+                        <p className="text-xl">No song currently on queue</p>
+                    </div>
+                </div>
+            }
+
+            {popupWindow && <ConfirmDeletePopup popupFn={setPopupWindow} setQueueFn={setQueue} />}
+        </div>
+    )
+}
+
+const ConfirmDeletePopup = ({ popupFn, setQueueFn }: {
+    popupFn: Dispatch<SetStateAction<boolean>>
+    setQueueFn: Dispatch<SetStateAction<VideoListResponse>>
+}) => {
+    return (
+        <div className="w-full h-full fixed top-18 left-0 flex justify-center items-center bg-[hsla(0,100%,100%,0.2)]">
+            <div className="w-70 md:w-80 h-70 flex flex-col justify-evenly items-center rounded-2xl bg-background">
+                <h2 className="mx-5 text-center text-2xl font-bold">Delete all song queue?</h2>
+                
+                <div className="w-full flex justify-evenly [&>button]:border [&>button]:p-2 [&>button]:rounded-2xl">
+                    <button className="hover:bg-(--red-clr) hover:border-(--red-clr)"
+                        onClick={_ => {
+                            emptySongQueue()
+                            setQueueFn(getSongQueue())
+                            popupFn(false)
+                        }}
+                    >
+                        Delete
+                    </button>
+
+                    <button onClick={_ => popupFn(false)} className="hover:bg-foreground hover:text-background">
+                        Cancel
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
