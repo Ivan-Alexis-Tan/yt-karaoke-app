f157f15c44a851302bcb15c9975ef9e6e6f00b31
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-16 01:47:45 +0800

    feat (player): add control panel for user

--- frontend/app/play/[videoId]/MainPlayer.tsx
+++ frontend/app/play/[videoId]/MainPlayer.tsx
@@ -1,10 +1,14 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { getSongQueue } from "@/src/utils/queueFn";
+
 import KaraokePlayer from "./KaraokePlayer";
-import Recommendations from "./Recommendations";
-import QueueIcon from "@/src/svgs/QueueIcon";
 import ShowVideos from "@/app/ShowVideos";
+import SongNoteIcon from "@/src/svgs/SongNoteIcon";
+import SearchIcon from "@/src/svgs/SearchIcon";
+import HideIcon from "@/src/svgs/HideIcon";
 
 type MainPlayerProps = {
     videoId: string
@@ -12,34 +16,62 @@ type MainPlayerProps = {
     className?: string 
 }
 
+type PaneKeys = "none" | "songs" | "search"
+
 export default function MainPlayer({ videoId, videoList, className }: MainPlayerProps) {
-    const [showReccom, setShowReccom] = useState(true)
-    return (
-        <div className={`${className ?? ""}`}>
-            <div className="mb-5 gap-3 flex justify-end">
-                <button onClick={_ => setShowReccom(p => !p)}>
-                    Songs
+    const [showPane, setShowPane] = useState<PaneKeys>("songs")
+    const [songQueue, setSongQueue] = useState(getSongQueue())
+
+    useEffect(() => console.log(`showPane =`, showPane), [showPane])
+
+    const RightPaneControls = ({ className }: { className?: string }) => {
+        return (
+            <div className={`${className ?? ""} [&>button]:flex [&>button]:gap-1 [&>button]:items-center [&>button]:hover:border-foreground [&>button]:transition-all`}>
+                <button onClick={_ => setShowPane("search")}
+                    className={`${showPane === "search" && "bg-foreground text-background"} px-1 border border-background rounded`}
+                >
+                    <SearchIcon className="w-7 h-7" />
+                    <p>Search</p>
                 </button>
 
-                <button>
-                    <QueueIcon className="w-10 h-10" />
+                <button onClick={_ => setShowPane("songs")}
+                    className={`${showPane === "songs" && "bg-foreground text-background"} px-1 border border-background rounded`}
+                >
+                    <SongNoteIcon className="w-7 h-7" />
+                    <p>Songs</p>
+                </button>
+
+                <button onClick={_ => setShowPane("none")}
+                    className={`${showPane === "none" && "bg-foreground text-background"} px-1 border border-background rounded hover:text-(--red-clr)`}
+                >
+                    <HideIcon className="w-7 h-7" />
+                    <p>Hide</p>
                 </button>
             </div>
+        )
+    }
 
-            <div className={`${showReccom && "gap-3 grid grid-cols-[2fr_1fr]"}`}>
+    return (
+        <div className={`${className ?? ""} ${showPane !== "none" && "gap-3 grid grid-cols-1 lg:grid-cols-[2fr_1fr]"}`}>
+            <div>
                 <KaraokePlayer videoId={videoId} />
 
-                {showReccom
-                    && <div className="h-135 overflow-auto">
-                        <ShowVideos 
-                            
-                            videoList={videoList} 
-                        />
-                    </div>
-                }
+                <RightPaneControls className="mx-5 mb-5 gap-3 flex justify-end" />
             </div>
 
-            
+            {showPane === "songs"
+                && <div className="overflow-auto">
+                    <ShowVideos className="[&_a.karaoke-video-card]:mx-5 lg:[&_div.karaoke-video-card]:mx-0 lg:[&_div.karaoke-video-card]:mr-5"
+                        videoList={videoList} 
+                    />
+                </div>
+            }
+
+            {showPane === "search"
+                && <div>
+
+                </div>
+            }
         </div>
     )
 }
