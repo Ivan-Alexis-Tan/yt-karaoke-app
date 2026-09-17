6c4c95c02f4e7c9ca3c411ba3529b0c4c6b372d3
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-18 01:45:13 +0800

    feat (component): add spinner component

---
+++ frontend/src/components/Spinner.tsx
@@ -0,0 +1,12 @@
+export default function Spinner() {
+    return <div className="
+        size-6
+        animate-spin 
+        border-3 
+        border-t-(--red-clr) 
+        border-r-(--red-clr)
+        border-b-transparent
+        border-l-transparent
+        rounded-full
+    "/>
+}
