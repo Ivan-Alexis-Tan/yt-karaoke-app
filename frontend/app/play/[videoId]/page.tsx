772f3750d7e9919457f569f196163c46e2fe4c91
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-18 23:30:30 +0800

    refactor (player): stylize responsive UI and remove unnecessary imports

--- frontend/app/play/[videoId]/page.tsx
+++ frontend/app/play/[videoId]/page.tsx
@@ -1,6 +1,4 @@
-import KaraokePlayer from "./KaraokePlayer"
-import { cacheToHistory, getRandomVideos, getVideo } from "@/src/api/videosApi";
-import ShowVideos from "@/app/ShowVideos";
+import { cacheToHistory, getRandomVideos } from "@/src/api/videosApi";
 import MainPlayer from "./MainPlayer";
 
 type PlayerPageProps = {
@@ -9,12 +7,11 @@ type PlayerPageProps = {
 
 export default async function PlayerPage({ params }: PlayerPageProps) {
     const videoId = (await params).videoId;
-    const videoDetails = await getVideo(videoId)
     const randomVideos = await getRandomVideos()
     await cacheToHistory(videoId)
 
     return (
-        <div className="h-[calc(100vh-75px)] sm:h-[calc(100vh-87px)] md:sm:h-[calc(100vh-75px)]">
+        <div className="lg:h-[calc(100vh-75px)]">
             <MainPlayer 
                 videoId={videoId}
                 videoList={randomVideos}
