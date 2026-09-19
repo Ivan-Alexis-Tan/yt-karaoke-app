36aa5bba9c2a7f247a74af911f4091844de712d2
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-19 19:19:19 +0800

    feat (utils): add saving function for search details

--- frontend/src/utils/useCurrentVideo.ts
+++ frontend/src/utils/useCurrentVideo.ts
@@ -3,18 +3,32 @@ import { getVideo } from "../api/videosApi";
 
 type UseVideosStates = {
     currentSong: VideoListResponse[number] | null
-    addCurrentSong: (video_id: VideoIdType) => void
+    searchKey: string
+    searchVideoList: VideoListResponse
+    updateCurrentSong: (videoId: VideoIdType) => void
+    updateVideoList: (videoList: VideoListResponse) => void
+    updateSearchKey: (key: string) => void
 }
 
 export const useCurrentVideo = create<UseVideosStates>((set) => ({
     currentSong: null,
-    addCurrentSong: async (video_id: VideoIdType) => {
-        const fetched = await getVideo(video_id)
+    searchKey: "",
+    searchVideoList: [],
+    updateCurrentSong: async (videoId: VideoIdType) => {
+        const fetched = await getVideo(videoId)
         set(_ => ({ currentSong:  fetched }) )
     },
+    updateVideoList: (videoList: VideoListResponse) => set(s => ({ ...s, searchVideoList: videoList}) ),
+    updateSearchKey: (key: string) => set(s => ({ ...s, searchKey: key }) ),
 }))
 
 export const currentSongSelector = (state: UseVideosStates) => state.currentSong
-export const addCurrentSongSelector = (state: UseVideosStates) => state.addCurrentSong
+export const addCurrentSongSelector = (state: UseVideosStates) => state.updateCurrentSong
+
+export const searchKeySelector = (state: UseVideosStates) => state.searchKey
+export const updateSearchKeySelector = (state: UseVideosStates) => state.updateSearchKey
+
+export const videoListSelector = (state: UseVideosStates) => state.searchVideoList
+export const updateVideoListSelector = (state: UseVideosStates) => state.updateVideoList
 
 export default useCurrentVideo
