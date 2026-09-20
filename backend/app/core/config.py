2490f46101201d70a7a3f5227b636c89539cb8b3
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-09-20 23:59:30 +0800

    feat (config): add APP_ENVIRONMENT `.env` reader

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,7 +7,8 @@ env_path = current_path / ".env"
 
 load_dotenv(env_path)
 
-DATABASE_URL = os.getenv("LOCAL_DATABASE_URL")
+APP_ENVIRONMENT = os.getenv("APP_ENVIRONMENT")
+DATABASE_URL = os.getenv("DATABASE_URL") if APP_ENVIRONMENT == "PRODUCTION" else os.getenv("LOCAL_DATABASE_URL")
 API_KEY = os.getenv("YOUTUBE_API_KEY")
 YOUTUBE_URL = "https://www.googleapis.com/youtube/v3"
 
